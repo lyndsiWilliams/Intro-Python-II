cdb039dabe74e959d0df23515b52d687a1096243
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-08 18:17:15 -0500

    Correct input of directions working, incorrect still breaks

--- src/player.py
+++ src/player.py
@@ -5,3 +5,9 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+    
+    def introduce_character(self):
+        print(f'You hear a distant voice, "Open your eyes, {self.name}..."')
+
+    def room_description(self):
+        print(f'\nYou are at the {self.current_room.name}.\n{self.current_room.description}.')
